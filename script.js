79330a4129533ce8b40a5962599cab4984214637
amandapacchacommunity <pacchaamanda@gmail.com>
2026-03-18 15:07:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,27 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 fetch('data/neighborhoods.geojson')
-  .then(response => response.json())
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Could not load neighborhoods.geojson');
+    }
+    return response.json();
+  })
   .then(data => {
-    L.geoJSON(data, {
+    const layer = L.geoJSON(data, {
       style: function(feature) {
         return {
           color: "#333",
+          weight: 1,
           fillColor: "#69b3a2",
-          fillOpacity: 0.5
+          fillOpacity: 0.4
         };
       }
     }).addTo(map);
+
+    map.fitBounds(layer.getBounds());
+  })
+  .catch(error => {
+    console.error('GeoJSON load error:', error);
+    alert('Could not load neighborhood boundaries. Check that data/neighborhoods.geojson exists.');
   });
